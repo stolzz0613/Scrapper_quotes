ded6295f76c137d9e8b0b9f3550503a272448f70
Andres <andres0613@MacBook-Air-de-Andres.local>
2021-10-09 00:57:53 -0500

    Project finished

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -12,31 +12,47 @@ class QuotesScrapy(scrapy.Spider):
     ]
     custom_settings = {
         'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
+        'FEED_FORMAT': 'json',
+        'CONCURRENT_REQUEST': 24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['andres0613@utp.edu.co'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'andresB',
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
     def parse_only_quotes(self, response, **kwargs):
         if kwargs:
             quotes = kwargs['quotes']
+            authors = kwargs['authors']
+
         quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+        authors.extend(response.xpath('//small[@class="author" and @itemprop="author"]/text()').getall())
         next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
         else:
             yield {
-                'quotes': quotes
+                'quotes': list(zip(quotes, authors))
             }
     
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        authors = response.xpath('//small[@class="author" and @itemprop="author"]/text()').getall()
+        top_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
         
+        top = getattr(self, 'top', None)
+
+        if top:
+            top = int(top)
+            top_tags = top_tags[:top]
+
         yield {
             'title': title,
-            'top_ten_tags': top_ten_tags
+            'top_tags': top_tags
         } 
 
         next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
